23899f23b5fd0e05c5811545732822f1575c00a6
Sachsenspielt96 <sachsenspielt3@gmail.com>
2022-01-04 17:24:32 +0100

    Cleaned up Server.java a bit
    Signed-off-by: Sachsenspielt96 <sachsenspielt3@gmail.com>

--- server/src/main/java/de/ppp/vr/server/Server.java
+++ server/src/main/java/de/ppp/vr/server/Server.java
@@ -2,12 +2,9 @@ package de.ppp.vr.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -17,8 +14,6 @@ public class Server {
         MainUI.init();
         SpringApplication.run(Server.class, args);
     }
-
-    private static final String template = "Hello, %s!";
 
     @PostMapping("/sendcoords")
     public String greeting(@RequestBody Coords coords) {
