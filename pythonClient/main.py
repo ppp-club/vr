0a8a8a8a3e833132c5d1acd2afa7aceb8120d7eb
Marshi <Marshi.Dev@gmail.com>
2022-01-18 17:37:07 +0100

    Client

--- pythonClient/main.py
+++ pythonClient/main.py
@@ -4,10 +4,12 @@ import threading
 import time
 import json
 import requests
+import tkinter as tk
 
 pyautogui.FAILSAFE = True
 
-ip = input("HOST: ")
+#ip = input("HOST: ")
+ip = "192.168.151.222"
 port = input("PORT: ")
 
 class mb:
@@ -33,7 +35,7 @@ def updatePos(mbutton):
         minus["x"] = pyautogui.position().x
         minus["y"] = pyautogui.position().y
 
-    time.sleep(0.01)
+        time.sleep(0.001)
 
 def on_click(x, y, button, pressed):
     if pressed:
@@ -48,9 +50,28 @@ def run1():
     with Listener(on_click=on_click) as listener:
         listener.join()
 
+tempBounds = requests.get("http://"+ip+":"+port+"/bounds", headers=headers)
+tempBounds = tempBounds.json()
+WIDTH = int(tempBounds["width"])
+HEIGHT = int(tempBounds["height"])
+
+def run2():
+    i = 0
+    while True:
+        updatePos(mb.mbutton)
+        if i == 0:
+            print("Successfully connected!")
+        i = 1
+
 print("Connecting to Server...")
 thread1 = threading.Thread(target=run1)
-thread1.start()
-print("Successfully connected!")
-while True:
-    updatePos(mb.mbutton)
+try:
+    thread1.start()
+except:
+    print("failed")
+thread2 = threading.Thread(target=run2)
+thread2.start()
+
+root = tk.Tk(className="Drawing-pad")
+root.geometry(str(WIDTH) + "x" + str(HEIGHT))
+root.mainloop()
